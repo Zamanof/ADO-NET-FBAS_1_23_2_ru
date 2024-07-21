b43c0774e2bb963937bec67cbfc50a243a1442e8
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2024-07-21 15:23:00 +0400

    Models. Annotations 1

---
+++ ADO 07. Annotations/Teacher.cs
@@ -0,0 +1,12 @@
+﻿namespace ADO_07._Annotations;
+
+internal class Teacher
+{
+    public int TeacherId {  get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDay { get; set; }
+    public float Salary {  get; set; }
+
+    public float Bonus {  get; set; }
+}
